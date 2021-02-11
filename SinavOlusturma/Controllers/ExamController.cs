21f8ccb841dbbf7f70279c45a69428d755216556
Doğan Aker <m.doganaker@gmail.com>
2021-02-11 18:09:40 +0300

    Exam page designed and contests of selected exam brought to the page.

---
+++ SinavOlusturma/Controllers/ExamController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SinavOlusturma.Models.ORM.Context;
+using SinavOlusturma.Models.ORM.Entities;
+using SinavOlusturma.Models.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SinavOlusturma.Controllers
+{
+    public class ExamController : Controller
+    {
+        private readonly ExamContext _examContext;
+
+        public ExamController(ExamContext examContext)
+        {
+            _examContext = examContext;
+        }
+        public IActionResult Exam(int id)
+        {
+            UserExamVM userExam = new UserExamVM();
+            Exam exam = _examContext.Exams.Include(q => q.QuestionOne).Include(q => q.QuestiontWO).Include(q => q.QuestionThree).Include(q => q.QuestionFour).FirstOrDefault(x => x.ID == id && x.IsDeleted == false);
+            
+            userExam.ArticleTitle = exam.ArticleTitle;
+            //userExam.ArticleContent = content;
+            userExam.Q1 = exam.QuestionOne;
+            userExam.Q2 = exam.QuestiontWO;
+            userExam.Q3 = exam.QuestionThree;
+            userExam.Q4 = exam.QuestionFour;
+
+            return View(userExam);
+        }
+
+        public IActionResult Answers(int id)
+        {
+            Question question = _examContext.Questions.Find(id);
+            return Json(question);
+        }
+
+    }
+}
---
+++ SinavOlusturma/Models/VM/UserExamVM.cs
@@ -0,0 +1,18 @@
+﻿using SinavOlusturma.Models.ORM.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SinavOlusturma.Models.VM
+{
+    public class UserExamVM
+    {
+        public string ArticleTitle { get; set; }
+        public string ArticleContent { get; set; }
+        public Question Q1 { get; set; }
+        public Question Q2 { get; set; }
+        public Question Q3 { get; set; }
+        public Question Q4 { get; set; }
+    }
+}
